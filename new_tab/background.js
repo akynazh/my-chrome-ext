bd20d6962d15a838dd769a36096e589fa6336af7
akynazh <akynazh@qq.com>
2023-03-03 17:56:20 +0800

    tabGroup new tab

--- new_tab/background.js
+++ new_tab/background.js
@@ -1,3 +1,22 @@
 chrome.action.onClicked.addListener(() => {
-	chrome.tabs.create({"url": "chrome://newtab"})
+  // create new tab
+  // chrome.tabs.create({"url": "chrome://newtab"});
+
+  // create new tab in "ing" group
+  chrome.tabGroups.query({}, function (groups) {
+    // Loop through all tab groups
+    for (var i = 0; i < groups.length; i++) {
+      var group = groups[i];
+      // Check if this is the desired group
+      if (group.title === "ing") {
+        // Get the group's ID and move the tab to the group
+        var groupId = group.id;
+        chrome.tabs.create({ url: "chrome://newtab" }, function (tab) {
+          chrome.tabs.group({ tabIds: [tab.id], groupId: groupId });
+        });
+        // Exit the loop since we've found the desired group
+        break;
+      }
+    }
+  });
 });
